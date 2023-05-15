27b86901dc20a45f5cef827eb7df97294265a5d0
yoloytZz <2311170320@qq.com>
2023-05-15 20:35:52 +0800

    a small thank for git

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,7 @@ using namespace std;
 int main(int argc, char *argv[])
 {
     std::cout << "Hello world!" << std::endl;
+    // thank you for your help
+    cout << "thank" << endl;
     return 0;
 }
